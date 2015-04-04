0ed0464995783f96108542d8428c1741db7ed35d
Jared Baker <jbaksta@gmail.com>
2015-04-04 11:52:34 -0600

    restructure, use M_PI

--- src/sunDeclination.c
+++ src/sunDeclination.c
@@ -1,14 +1,21 @@
+/*
+File: sunDeclination.c
+
+ChangeLog
+2015.04.04 JDB Restructured code, used M_PI from POSIX
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include "sunDeclination.h"
 
+void sunDeclination(
+		double *delta, 
+		int const day
+		) {
 
-
-void sunDeclination(double *delta, int day){
-	*delta = ((23.45*4*atan(1))/180)*cos(((2*4*atan(1))/365)*(172-day));
-
-	return;
+	*delta = (23.45*M_PI/180.0)*cos((2*M_PI/365.0)*(172-day));
 
 }

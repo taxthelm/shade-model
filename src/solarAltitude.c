07c6939581f44c61067ca27ccbf7ac556318c282
Jared Baker <jbaksta@gmail.com>
2015-04-04 11:44:10 -0600

    structure of code

--- src/solarAltitude.c
+++ src/solarAltitude.c
@@ -2,9 +2,16 @@
 
 #include "solarAltitude.h"
 
-void solarAltitude(double * solarAlt, double sunDeclination, double latitude, double localHourAngle)
+void solarAltitude(
+		double *solarAlt,
+		double const sunDeclination, 
+		double const latitude, 
+		double const localHourAngle)
 {
 	//Equation for solar altitude as found on page 25 of reading (Planetary Motions and the Distribution of Radiation)
-	*solarAlt = asin(sin(sunDeclination)*sin(latitude) + cos(sunDeclination)*cos(latitude)*cos(localHourAngle));
+	*solarAlt = asin( 
+			sin(sunDeclination)*sin(latitude) + 
+			cos(sunDeclination)*cos(latitude)*cos(localHourAngle)
+			);
 		
 }

4999c4fc02723c86d86a770a28a91964e6997a16
Jared Baker <jbaksta@gmail.com>
2015-04-04 20:44:44 -0600

    add const keywords

--- include/azimuth.h
+++ include/azimuth.h
@@ -1,12 +1,15 @@
+/*
+   Solar azimuth angle calculated from North. Azimuth angle will
+   be negative before noon.
+*/
+
 #ifndef AZIMUTH_H
 #define AZIMUTH_H
 
-// Solar azimuth angle calculated from North
-// Azimuth angle will be negative before noon
 // All input angles should be in radians
 void azimuth(double *azimuth, 			// solar azimuth angle (output)
-			 double solarAlt,			// solar altitude
-			 double latitude,			// latitude in radians, negative latitude implies south of equator
-			 double declination, 		// solar declination angle
+			 double const solarAlt,		// solar altitude
+			 double const latitude,		// latitude in radians, negative latitude implies south of equator
+			 double const declination, 	// solar declination angle
 			 int time);					// denotes AM/PM with 0 for AM and 1 for PM
 #endif

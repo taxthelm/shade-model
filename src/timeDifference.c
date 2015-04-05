2880398f6017fb35ee0727d470c6cfb19e4c5525
Jared Baker <jbaksta@gmail.com>
2015-04-04 18:47:37 -0600

    simple optimizations

--- src/timeDifference.c
+++ src/timeDifference.c
@@ -1,21 +1,33 @@
+/*
+File: timeDifference.c
+
+ChangeLog
+
+2015.04.04 JDB	swapped the order of thetaS and thetaL and calculated 1.0/15.0 
+				to be a constant. Remember, divisions can be expensive :)
+
+Original: 
 //Bill Matonte
 //6.27.13
 //REU SUMMER 2013 
 //timedifference.c
 //This function finds the offset for the time of day for the solar cumilative 
-//radiation formula. 
+//radiation formula.
+*/
+
 #include "timeDifference.h"
-#include <stdio.h>
 //The Variables ThetaS and ThetaL with the pointer to the deltaT
 //see timedifference.h
 //thetaS = standard time meridian longitde of time zone
 //thetaL = local longitude
 
-void timeDifference(double* deltaT1, double thetaS,double thetaL)
-{
-	*deltaT1=-( thetaS-thetaL)/15.0;
-	return;
+void timeDifference(
+		double* deltaT1, 
+		double const thetaS,
+		double const thetaL
+		) {
+	
+	*deltaT1=  0.06666666666666666*(thetaL-thetaS);
+
 }
-
-
 

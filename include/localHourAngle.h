4f51d4c5492fa1c5329ae38fe1142934ace0c720
Jared Baker <jbaksta@gmail.com>
2015-04-04 20:55:42 -0600

    added const keywords

--- include/localHourAngle.h
+++ include/localHourAngle.h
@@ -1,18 +1,31 @@
-/* REU HPC Summer 2013 
- * Program Purpose: 
- * Created by: Noll Roberts
- * On: 2013.06.20
- * Modified by:
- * On:
- * */
+/*
+File: localHourAngle.h
+
+ChangeLog:
+
+2015.04.04 JDB	Added const keywords
+
+
+Original:  
+  REU HPC Summer 2013 
+  Program Purpose: 
+  Created by: Noll Roberts
+  On: 2013.06.20
+  Modified by:
+  On:
+
+*/
 
 #ifndef LAL_H 
 #define LAL_H 
 
-/**************** Linear Algebra Functions ************************/
-
 // Returns tao, the local hour angle. (in degrees) 
-void localHourAngle( double *tau, double Ts, double deltaT1, double deltaT2, int timeStep);
-
+void localHourAngle( 
+		double *tau, 
+		double const Ts, 
+		double const deltaT1, 
+		double const deltaT2, 
+		int const timeStep
+		);
 
 #endif

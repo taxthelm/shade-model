70eaba1a32fb02a238e6d1a620761473fa559de8
Jared Baker <jbaksta@gmail.com>
2015-04-04 21:07:45 -0600

    added const keywords

--- include/tilt.h
+++ include/tilt.h
@@ -1,9 +1,20 @@
+/*
+File: tilt.h
+
+ChangeLog
+2015.04.04 JDB	Added const keywords
+
+*/
+
 #include "landStruct.h"
 #ifndef TILT_H
 #define TILT_H
 
-void tilt(LandData *mainPlot, 				// plot of land tilt angles are calculated for
-		  LandData *rightNeighbor, 			// plot of land directly to the right of the main plot
-		  LandData *lowerNeighbor,			// plot of land directly below the main plot
-		  int upperLower);					// tag for upper or lower trianglular plot. 1 denotes upper
-#endif
+void tilt(
+		LandData *mainPlot, 		// plot of land tilt angles are calculated for
+		LandData *rightNeighbor, 	// plot of land directly to the right of the main plot
+		LandData *lowerNeighbor,	// plot of land directly below the main plot
+		int const upperLower		// tag for upper or lower trianglular plot. 1 denotes upper
+		);
+		
+#endif // TILT_H

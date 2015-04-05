a8748fb03786c94e5b78aeccc649d7fc43f8ee3f
Jared Baker <jbaksta@gmail.com>
2015-04-04 21:04:25 -0600

    added const keywords

--- include/sunDeclination.h
+++ include/sunDeclination.h
@@ -1,13 +1,24 @@
+/*
+File: sunDeclination.c
+
+ChangeLog
+2015.04.04 JDB	Added const keywords
+
+
+Contents:
+
+sunDeclination()	Calculates the angle of the sun above or below
+					the equator on a given day of the year.
+
+*/
+
+
 #ifndef MAT_H
 #define MAT_H
 
-/*
-* This function calculates the angle of the sun above or below the equator
-* on a given day of the year. 
-*/
-
-void sunDeclination(double *delta, int D);
-
-
+void sunDeclination(
+		double *delta, 
+		int const D
+		);
 
 #endif

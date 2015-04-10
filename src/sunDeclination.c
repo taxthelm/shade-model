0f3d3c5d7b8872d9aa649adbb35808616c3c5bd9
Jared D. Baker <jbaksta@gmail.com>
2015-04-09 18:52:32 -0600

    small math optimization

--- src/sunDeclination.c
+++ src/sunDeclination.c
@@ -3,6 +3,7 @@ File: sunDeclination.c
 
 ChangeLog
 2015.04.04 JDB Restructured code, used M_PI from POSIX
+2015.04.09 JDB Replaced some constants
 */
 
 #include <stdio.h>
@@ -16,6 +17,8 @@ void sunDeclination(
 		int const day
 		) {
 
-	*delta = (23.45*M_PI/180.0)*cos((2*M_PI/365.0)*(172-day));
+//	*delta = (23.45*M_PI/180.0)*cos((2*M_PI/365.0)*(172-day));
+	*delta = 0.40927970959267024*cos(0.017214206321039961*(172-day));
+
 
 }
